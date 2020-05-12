04b1fb451c32b81ab01ba8f9a254aba0d6e79eb4
Anurag-Joshi-B1 <65150509+Anurag-Joshi-B1@users.noreply.github.com>
2020-05-12 15:02:40 +0530

    Problem 2:- Given an integer array A of N integers, find the pair of integers in the array which have minimum XOR value. Report the minimum XOR value

---
+++ Anurag Joshi (cse - B1)/Date - 11-may-2020/xor.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+using namespace std; 
+
+int main(){
+	int arr[] = {1,14,6,9,4};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	
+	sort(arr, arr + n);
+
+	int val = 0,min = arr[0]^arr[1];
+	
+	for (int i = 0; i < n - 1; i++) {
+		for(int j = 0;j<n-1;j++){
+			val = arr[i] ^ arr[i + 1];
+			if(min>val){
+				min = val;
+			}
+		}
+	} 
+
+	cout<<"Minimum XOR value: "<<min;
+	return 0;
+}
